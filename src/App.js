d3dd1eccbedd6d444a39f1705047af0da2c2b04a
Othmane Filali <othmane.filali@gmail.com>
2022-10-29 09:23:00 +0200

    Commit First

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Home from './pages/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import './Home.css'
+const Home = () => {
+    return (
+        <div className="container-fluid">
+           <p className='text-center text-warning'>Home Page</p>
+        </div>
+    )
+}
+
+export default Home
