4f231e460209ce0c318ef012a4a8bf895f3a8e62
Othmane Filali <othmane.filali@gmail.com>
2022-10-29 09:31:32 +0200

    Second Commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import './Home.css'
 const Home = () => {
     return (
-        <div className="container-fluid">
-           <p className='text-center text-warning'>Home Page</p>
+        <div className="container-fluid px-20 py-10">
+           <h3 className='text-center text-warning mt-5 text-uppercase'>shop page</h3>
+           <div className="container py-4">
+                <div className="row">
+                    
+                </div>
+           </div>
         </div>
     )
 }
